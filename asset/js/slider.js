2c67014dba7f6090e129f4a613de1f71db939dad
vuho <HoThiVu>
2022-08-31 08:45:16 +0700

    save

--- asset/js/slider.js
+++ asset/js/slider.js
@@ -1,7 +1,7 @@
 var swiper = new Swiper(".mySwiper", {
     loop: true,
     spaceBetween: 30,
-    slidesPerView: 6,
+    slidesPerView: 7,
     freeMode: true,
     watchSlidesProgress: true,
   });
@@ -16,3 +16,44 @@ var swiper = new Swiper(".mySwiper", {
       swiper: swiper,
     },
   });
+
+  // ----tabs
+  function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("city");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// ------------ tabs slide
+function openSlide(evt, sl) {
+  var i, tabcontent, tabslide;
+  tabcontent = document.getElementsByClassName("");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+  tabslide = document.getElementsByClassName("tabslide");
+  for (i = 0; i < tabslide.length; i++) {
+    tabslide[i].className = tabslide[i].className.replace(" ac", "");
+  }
+  document.getElementById(sl).style.display = "block";
+  evt.currentTarget.className += " ac";
+}
+// ------------------- quantity add to cart
+function plus() {
+  var qty = document.getElementById("qty-input").value;
+  document.getElementById('qty-input').value =parseInt(qty)+1;
+}    
+
+function minus() {
+  var qty = document.getElementById("qty-input").value;
+  if(parseInt(qty)>1)
+  document.getElementById('qty-input').value =parseInt(qty)-1;
+}  
