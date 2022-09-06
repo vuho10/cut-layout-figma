db32b46f68186a87b6c20bdf76af8d5a1766a0b5
vuho <HoThiVu>
2022-09-07 06:42:12 +0700

    save

--- asset/js/slider.js
+++ asset/js/slider.js
@@ -1,35 +1,35 @@
 var swiper = new Swiper(".mySwiper", {
-    loop: true,
-    spaceBetween: 30,
-    slidesPerView: 6,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".mySwiper2", {
-    loop: true,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper,
-    },
-  });
+  loop: true,
+  spaceBetween:14,
+  slidesPerView: 6,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
 
-  // ----tabs
-  function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("city");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+// ----tabs
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("city");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
 // ------------ tabs slide
@@ -37,7 +37,7 @@ function openSlide(evt, sl) {
   var i, tabcontent, tabslide;
   tabcontent = document.getElementsByClassName("");
   for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+    tabcontent[i].style.display = "none";
   }
   tabslide = document.getElementsByClassName("tabslide");
   for (i = 0; i < tabslide.length; i++) {
@@ -49,11 +49,11 @@ function openSlide(evt, sl) {
 // ------------------- quantity add to cart
 function plus() {
   var qty = document.getElementById("qty-input").value;
-  document.getElementById('qty-input').value =parseInt(qty)+1;
-}    
+  document.getElementById('qty-input').value = parseInt(qty) + 1;
+}
 
 function minus() {
   var qty = document.getElementById("qty-input").value;
-  if(parseInt(qty)>1)
-  document.getElementById('qty-input').value =parseInt(qty)-1;
+  if (parseInt(qty) > 1)
+    document.getElementById('qty-input').value = parseInt(qty) - 1;
 }  
