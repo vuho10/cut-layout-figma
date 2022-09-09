22c220cf8a10b31d5c7b10dcf3ea3d1ca7787ed1
vuho <HoThiVu>
2022-09-09 15:57:40 +0700

    save

--- asset/js/slider.js
+++ asset/js/slider.js
@@ -1,8 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
   loop: true,
-  spaceBetween: 14,
+  spaceBetween: 12,
   slidesPerView: 6,
-  freeMode: true,
+  // freeMode: true,
   watchSlidesProgress: true,
 });
 var swiper2 = new Swiper(".mySwiper2", {
@@ -17,37 +17,6 @@ var swiper2 = new Swiper(".mySwiper2", {
   },
 });
 
-// ----tabs
-function openTab(evt, tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tab");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-// hover btn cart
-function hoverBtn(evt, tabCart) {
-  var i, tabHover, tabbtn;
-  tabHover = document.getElementsByClassName("hoverB");
-  for (i = 0; i < tabHover.length; i++) {
-    tabHover[i].style.display = "none";
-  }
-  tabbtn = document.getElementsByClassName("tabbtn");
-  for (i = 0; i < tablinks.length; i++) {
-    tabbtn[i].className = tabbtn[i].className.replace(" backgr", "");
-  }
-  document.getElementById(tabCart).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-
 // ------------------- quantity add to cart
 function plus() {
   var qty = document.getElementById("qty-input").value;
@@ -59,3 +28,27 @@ function minus() {
   if (parseInt(qty) > 1)
     document.getElementById('qty-input').value = parseInt(qty) - 1;
 }  
+
+
+// ----tabs
+function openTab(evt, tabName) {
+  var i, tab, tablinks;
+  tab = document.getElementsByClassName("tab");
+ 
+  for (i = 0; i < tab.length; i++) {
+    tab[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", " ");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+ 
+}
+document.getElementById("defaultOpen").click();
+
+
+
+
